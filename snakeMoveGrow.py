a2e1bdac26a73f72ecbc715ad3e96eeb149dc257
epstei17 <mark.epstein@mail.utoronto.ca>
2019-10-27 12:25:17 -0400

    Move and grow methods (incomplete)

---
+++ snakeMoveGrow.py
@@ -0,0 +1,15 @@
+import linked_list as snake
+
+def move_snake(snake,'input'):
+    #WASD
+
+    if 'input' == W:
+        snake._first = #TODO 
+
+
+
+def grow_snake(snake, target_position):
+
+    if snake.head == target_position:
+
+        snake.tail.append(node)
