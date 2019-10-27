6975e8f620d57907040af98f66cbbcab36e57918
epstei17 <mark.epstein@mail.utoronto.ca>
2019-10-27 12:03:07 -0400

    testing push

---
+++ snakeMoveGrow.py
@@ -0,0 +1,11 @@
+def move_snake('input'):
+    #WASD
+
+    if 'input' == W:
+        snake.head =  
+
+
+
+def grow_snake(snake, current_position):
+    if snake.head == current_position:
+        snake.tail.append_node
